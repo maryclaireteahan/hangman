39b1c0fb28c2a687cdfdf5119c958428742146a0
maryclaireteahan <maryclaireteahan@gmail.com>
2023-09-24 18:00:19 +0100

    feat: creat function to pick random word from word list

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
 import random
 from words import word_list
+
+
+print("\nWelcome to HANGMAN!")
+print("-------------------")
+
+def pick_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+word = pick_word()
