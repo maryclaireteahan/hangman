7855037c53a0b91847aa65f3880ca665d14b4d16
maryclaireteahan <maryclaireteahan@gmail.com>
2023-09-26 20:40:10 +0100

    feat: add color to win/lose messages

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import random
 from words import word_list
 from colorama import Fore, Back, Style
 
-
 import pyfiglet
 
 ASCII_art_1 = pyfiglet.figlet_format("HANGMAN")
@@ -22,7 +21,6 @@ print(
 
 print(Style.RESET_ALL)
 
-
 def dashes():
     print(Style.NORMAL + Fore.CYAN
           + "=========================================")
@@ -30,7 +28,6 @@ def dashes():
 
 
 print("\nWelcome to HANGMAN!")
-dashes()
 
 
 def pick_word():
@@ -48,32 +45,34 @@ def main_menu():
         play_now = input("Y/N\n" + Style.NORMAL + Fore.GREEN).upper()
         print(Style.RESET_ALL)
         if play_now == "Y":
+            dashes()
             before_rules = True
             while before_rules:
-                dashes()
                 print("\nWould you like to read the rules? " + Style.NORMAL + Fore.GREEN)
                 game_rules = input("Y/N\n" + Style.NORMAL + Fore.GREEN).upper()
                 print(Style.RESET_ALL)
                 if game_rules == "Y":
+                    dashes()
                     rules()
                 elif game_rules == "N":
                     dashes()
-                    print(+"\nLet's play!\n")
+                    print("\nLet's play!\n")
                     play_game(word)
                 else:
                     invalid()
+                    dashes()
                     
         elif play_now == "N":
             print("\nThat's too bad, adios!\n")
             dashes()
         else:
             invalid()
+            dashes()
 
 
 def rules():
-    dashes()
     print("\nGreat! Listen up!\n")
-    print(Style.NORMAL + Fore.RED + Back.CYAN + "RULES")
+    print(Style.NORMAL + Fore.CYAN + "RULES")
     print(Style.RESET_ALL)
     print("\n1. Pick a letter.\n2. If the letter is in the word"
         + " you'll see it appear in the missing letters sections.\n3."
@@ -97,12 +96,12 @@ def rules():
             main_menu()
         else:
             invalid()
+            dashes()
 
 
 def invalid():
     print(Fore.RED + "\nThat isn't a valid option.")
     print(Style.RESET_ALL)
-    dashes()
 
 
 def lives_guesses(lives, letters_guessed):
@@ -127,7 +126,9 @@ def play_game(word):
                 lives_guesses(lives, letters_guessed)
                 dashes()
             elif guess in word:
-                print(f"\nGood choice! {guess} is in the word!\n")
+                print("\nGood choice!" 
+                      + Fore.GREEN + f" {guess} " 
+                      + Style.RESET_ALL + "is in the word!\n")
                 letters_guessed.append(guess)
                 correct_letters.append(guess)
                 correct_word = list(letters)
@@ -141,7 +142,8 @@ def play_game(word):
                 lives_guesses(lives, letters_guessed)
                 dashes()
             elif guess not in word:
-                print(f"\n{guess} isn't in the word\n")
+                print(Fore.RED + f"\n{guess} " 
+                      + Style.RESET_ALL + "isn't in the word\n")
                 letters_guessed.append(guess)
                 lives = lives - 1
                 lives_guesses(lives, letters_guessed)
@@ -153,15 +155,20 @@ def play_game(word):
                 + "\nTry again.\n"
             )
             dashes()
+
     if lives == 0:
-        print("\nSorry you lost.\n")
-        print(f"\nThe word was {word}.\n")
+        print(Fore.RED +"\nSorry you lost.\n" + Style.RESET_ALL)
+        print("\nThe word was" + Fore.RED +f" {word}.\n")
+        dashes()
         game_over()
     else:
-        print(
-            "\nCongratulations!"
-            + f"You guessed the word {word} with {lives} lives to go!\n"
-        )
+        print(Fore.GREEN +"\nCongratulations!" + Style.RESET_ALL)
+        print("You guessed the word" +Fore.GREEN 
+              + f" {word} "+ Style.RESET_ALL
+              + "with" +Fore.GREEN 
+              + f" {lives} " + Style.RESET_ALL
+              + "lives to go!\n")
+        dashes()
         game_over()
 
 
@@ -170,8 +177,8 @@ def game_over():
     while after_play:
         play_again = input("\nPlay again? Y/N\n").upper()
         if play_again == "Y":
-            print("\nGreat, let's play!\n")
             dashes()
+            print("\nGreat, let's play!\n")
             play_game(word)
         elif play_again == "N":
             print("\nLet's go you back to the main menu.\n")
